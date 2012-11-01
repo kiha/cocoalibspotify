3534d54276780beaeef0519a2c8a43c293f43cf6
Daniel Kennett <dan@spotify.com>
2012-11-01 15:43:37 +0100

    Fix iOS-only classes to use new threading methods
    Change-Id: I5349bf482fa1da66e66b7f091b7d84962d57eecd

--- iOS Library/View Controllers/SPFacebookPermissionsViewController.m
+++ iOS Library/View Controllers/SPFacebookPermissionsViewController.m
@@ -79,7 +79,7 @@
 #pragma mark - Actions
 
 -(void)add {
-	SPDispatchAsync(^{
+	dispatch_libspotify_async(^{
 		sp_signup_userdata_success success;
 		success.success = true;
 		sp_session_signup_perform_action(self.session.session, SP_SIGNUP_ACTION_CONNECT_TO_FACEBOOK_COMPLETED, &success);
@@ -91,7 +91,7 @@
 }
 
 -(void)cancel {
-	SPDispatchAsync(^{
+	dispatch_libspotify_async(^{
 		sp_signup_userdata_success success;
 		success.success = false;
 		sp_session_signup_perform_action(self.session.session, SP_SIGNUP_ACTION_CONNECT_TO_FACEBOOK_COMPLETED, &success);
